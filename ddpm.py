0d7acce44f4f6ede6d2f7c38515093eb03cfc89e
seoyoonkims <seoyoonkims@kaist.ac.kr>
2025-05-01 22:20:44 +0900

    training

--- ddpm.py
+++ ddpm.py
@@ -6,7 +6,7 @@ import math
 
 import datasets
 import argparse
-
+from datetime import datetime
 
 # Sinusoidal positional embedding
 def positional_embedding(x, emb_dim):
@@ -25,13 +25,12 @@ def positional_embedding(x, emb_dim):
 
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, emb_dim):
+    def __init__(self, input_dim, output_dim):
         super().__init__()
         self.input_dim = input_dim
-        self.emb_dim = emb_dim
         self.fc1 = nn.Linear(input_dim, 128)
         self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, emb_dim)
+        self.fc3 = nn.Linear(64, output_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -45,50 +44,88 @@ class PointDiffusionModel(nn.Module):
         super().__init__()
         self.input_dim = 3 * emb_dim
         self.emb_dim = emb_dim
-        self.mlp = MLP(self.input_dim, emb_dim)
+        self.mlp = MLP(self.input_dim, output_dim=2) # output_dim=2 for 2D points
 
     def forward(self, x, t):
-        return self.mlp(x)
+        # positional embedding
+        x_emb = torch.cat([positional_embedding(t, emb_dim=self.emb_dim), 
+                        positional_embedding(x[:, 0], emb_dim=self.emb_dim), 
+                        positional_embedding(x[:, 1], emb_dim=self.emb_dim)], dim=1)
+        return self.mlp(x_emb)
+
+
+class beta_scheduler(nn.Module):
+    def __init__(self, num_timesteps):
+        super().__init__()
+        self.num_timesteps = num_timesteps
+        self.betas = torch.linspace(0.0001, 0.02, num_timesteps)
+
+
+class NoiseScheduler(nn.Module):
+    def __init__(self, num_timesteps):
+        super().__init__()
+        self.num_timesteps = num_timesteps
+        self.betas = beta_scheduler(self.num_timesteps).betas
+        self.alphas = 1 - self.betas
+        self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
+    
+    def get_alpha_t(self, t):
+        return self.alphas_cumprod.gather(0, t)      # gather: 위치마다 다른 인덱싱
+    
+    def forward(self, x, t, noise):
+        alpha_t = self.get_alpha_t(t).unsqueeze(1)
+        return x * torch.sqrt(alpha_t) + noise * torch.sqrt(1 - alpha_t)
 
 
 def main(args):
-
     # Prepare the dataset
     dataset = datasets.get_dataset(args.dataset)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
 
-    # x 하나씩 꺼내서 밑에 넣어주기
-
-    # Model
-    t = torch.randint(0, args.num_timesteps, (1,))
     model = PointDiffusionModel(emb_dim=args.emb_dim)
+    noise_scheduler = NoiseScheduler(args.num_timesteps)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    for epoch in range(args.num_iteration):
+
+        # Training loop
+        model.train()
+        total_loss = 0        
+        for i, data in enumerate(dataloader):
+            # raw data
+            x = data[0]
+            t = torch.randint(0, args.num_timesteps, (x.size(0),), device=x.device)
+            
+            # add nosie
+            noise = torch.randn_like(x)
+            x_noisy = noise_scheduler(x, t, noise)
+            noise_pred = model(x_noisy, t)
+
+            # loss
+            loss = F.mse_loss(noise_pred, noise)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad()
+        
+            total_loss += loss.item()
+        
+        if epoch % 10 == 0:
+            avg_loss = total_loss / len(dataloader)
+            print(f"Epoch {epoch} completed. Average Loss: {avg_loss:.4f}")
+
+
+        # Evaluation
+        model.eval()
+        with torch.no_grad():
+            pass
     
-    # input
-    x = torch.cat([positional_embedding(t, emb_dim=args.emb_dim), 
-                    positional_embedding(x[:, 0], emb_dim=args.emb_dim), 
-                    positional_embedding(x[:, 1], emb_dim=args.emb_dim)])
-
-    # Add noise to the input
-    noise = torch.randn_like(x)
-    x_noisy = x + noise
-    # Pass through the MLP
-    x_pred = model(x_noisy)
-    
-    loss = F.mse_loss(x_pred, x)
-    loss.backward()
-    
-
-
-
-
-    # Noise Scheduler
-
-
-    # Optimizer
-
-
-    # Training Loop
-
+    # Storing the model
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    model_path = f"point_diffusion_model_{timestamp}.pt"
+    torch.save(model.state_dict(), model_path)
+    print(f"Model saved to {model_path}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Point diffusion model")
@@ -96,6 +133,7 @@ if __name__ == "__main__":
     parser.add_argument("--n", type=int, default=100, help="Number of samples")
     parser.add_argument("--emb_dim", type=int, default=128, help="Embedding dimension")
     parser.add_argument("--num_timesteps", type=int, default=50, help="Number of time steps")
+    parser.add_argument("--num_iteration", type=int, default=200, help="Number of iterations")
     args = parser.parse_args()
 
     main(args)
